f11ea2c421e082f89e713c823669b580229e5c35
mmj030703 <mayankjain7738@gmail.com>
2024-03-18 02:26:24 +0530

    Updated eslint rules

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import SearchResultItem from './SearchResultItem';
 import { YOUTUBE_SEARCH_VIDEOS_API_URL } from '../utils/constants';
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { YOUTUBE_POPULAR_VIDEOS_API_URL } from "../utils/constants";
 import VideoCard from "./VideoCard";
